3fef55b5fd56cf5bc5da8f6e8d28ebf654c5c792
Mohammed Hesham Farhan <moh.hesham.f@gmail.com>
2022-09-25 18:26:48 +0300

    Build the server and the DB connection

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const app = express();
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+
+// Database connection
+try {
+    mongoose.connect(process.env.DB_URI);
+    console.log("Database connected");
+} catch (err) {
+  console.log(err);
+}
+
+// Routes
+
+// Starting the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
