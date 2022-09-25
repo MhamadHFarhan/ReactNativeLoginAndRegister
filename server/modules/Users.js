08ac3b493489d92dcbe1671b2a6df5b5e15c2bec
Mohammed Hesham Farhan <moh.hesham.f@gmail.com>
2022-09-25 18:30:11 +0300

    Create Users schema

---
+++ server/modules/Users.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true, // This will add createdAt and updatedAt fields to the schema
+  }
+);
+
+// Hashing the password before saving it to the database
+UserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+module.exports = mongoose.model("User", UserSchema);
