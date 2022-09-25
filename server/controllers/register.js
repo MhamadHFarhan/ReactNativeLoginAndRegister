c18bf66b239567d4c2c6d2eb94009ab354b269cc
Mohammed Hesham Farhan <moh.hesham.f@gmail.com>
2022-09-25 18:44:38 +0300

    Make a redirect register to login

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -1,17 +1,14 @@
 const User = require("../modules/Users");
 
-const createUser = (req, res) => {
+const createUser = (req, res, next) => {
   const { username, password, email } = req.body;
 
   const user = new User({ username, password, email });
 
   user
     .save()
-    .then((user) => {
-      res.status(200).json({
-        message: "User created successfully",
-        user,
-      });
+    .then(() => {
+      next();
     })
     .catch((err) => {
       res.status(400).json({
--- server/routers/register.js
+++ server/routers/register.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { createUser } = require("../controllers/register");
+const login = require("../controllers/login");
 
 const registerRouter = express.Router();
 
-registerRouter.post("/", createUser);
+registerRouter.post("/", createUser, login);
 
 module.exports = registerRouter;
